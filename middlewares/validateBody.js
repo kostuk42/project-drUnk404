56f29e6e472a3c7a8d20bda5d830098271f70c07
kostiuchenkoo <kostiuchenkoo@upmc.edu>
2023-09-26 12:04:54 +0300

    corrected env.example to git, patch err handling

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,7 +2,12 @@ const {HttpError} = require('../helpers');
 const validateBody = (schema) => {
     const func = async (req, res, next) => {
         if(!req.body || Object.keys(req.body).length === 0) {
-            next(HttpError(400, 'missing fields'));
+            if( req.method === 'PATCH') {
+                next(HttpError(400, 'missing field favorite'));
+            } else {
+                next(HttpError(400, 'missing fields'));
+            }
+           return
         }
         const { error } = await schema.validate(req.body);
         if (error) {
