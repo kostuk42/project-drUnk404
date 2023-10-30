ae820c4e1ec7f1d18bb3884bb6e734ef76a0f123
millenialia <lenalebo@gmail.com>
2023-10-30 16:52:28 +0100

    small changes from main
    now all working properly

--- app.js
+++ app.js
@@ -5,23 +5,28 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 const contactsRouter = require('./routes/api/contacts');
+const app = express();
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
 const authRouter = require('./routes/api/auth');
 const filtersRouter = require('./routes/api/filters');
 const usersRouter = require('./routes/api/users');
 const drinksRouter = require('./routes/api/drinks');
 
-const app = express();
-
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'));
+app.use(express.static( 'public'));
 
-app.use('/users', authRouter)
-app.use('/api/contacts', contactsRouter)
+app.use('/auth', authRouter)
+app.use('/users', usersRouter)
+
+app.use('/api/contacts', contactsRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/filters', filtersRouter)
+app.use('/drinks', drinksRouter)
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
