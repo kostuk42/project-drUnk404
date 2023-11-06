d4bfd5e134c17964d3204f640cd5bfd47203a55c
Dmytro Stas <dima7stas777@gmail.com>
2023-11-05 20:22:38 -0500

    userUpdate response fix

--- controllers/users.js
+++ controllers/users.js
@@ -1,58 +1,57 @@
-const { User } = require('../models/user');
-const { HttpError, sendEmail} = require("../helpers");
-const { ctrlWrapper } = require("../helpers/ctrlWrapper")
+const { User } = require("../models/user");
+const { HttpError, sendEmail } = require("../helpers");
+const { ctrlWrapper } = require("../helpers/ctrlWrapper");
 
 const getCurrent = async (req, res) => {
-    const currentUser = req.user.toObject()
-    delete currentUser.password;
-    res.status(200).json(currentUser)
-}
+  const currentUser = req.user.toObject();
+  delete currentUser.password;
+  res.status(200).json(currentUser);
+};
 
 const sendSubscriptionEmail = async (req, res) => {
-    const { email } = req.body;
-    
-    const user = await User.findOne({ email });
-    if (!user) {
-        throw HttpError(404, 'User not found')
-    }
-    if(user.subscribe) {
-        throw HttpError(400, 'User is already subscribed.')
-    }
+  const { email } = req.body;
 
-    const subscriptionEmail = {
-        to: email,
-        subject: 'Welcome to DrUnk404 Newsletter!',
-        html: `<h3>We are thrilled to welcome you to the DrUnk404 community! Your subscription to our newsletter has been successfully processed.</h3>`
-    }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(404, "User not found");
+  }
+  if (user.subscribe) {
+    throw HttpError(400, "User is already subscribed.");
+  }
 
-    await sendEmail(subscriptionEmail);
-    user.subscribe = true
-    await user.save()
+  const subscriptionEmail = {
+    to: email,
+    subject: "Welcome to DrUnk404 Newsletter!",
+    html: `<h3>We are thrilled to welcome you to the DrUnk404 community! Your subscription to our newsletter has been successfully processed.</h3>`,
+  };
 
-    res.status(200).json({
-        message: 'Subscription Successful!'
-    })
-}
+  await sendEmail(subscriptionEmail);
+  user.subscribe = true;
+  await user.save();
+
+  res.status(200).json({
+    message: "Subscription Successful!",
+  });
+};
 
 const userUpdate = async (req, res) => {
+  const user = await User.findById(req.user.id);
 
-    const user = await User.findById(req.user.id);
-    
-    const newAvatarURL = req.file ? req.file.path : user.avatarURL;
-    const newUsername = req.body.username ? req.body.username : user.username;
+  const newAvatarURL = req.file ? req.file.path : user.avatarURL;
+  const newUsername = req.body.username ? req.body.username : user.username;
 
-    user.avatarURL = newAvatarURL;
-    user.username = newUsername;
+  user.avatarURL = newAvatarURL;
+  user.username = newUsername;
 
-    await user.save();
-    res.status(200).json({
-        message: 'User successfully updated.'
-    });
-}
+  await user.save();
 
+  const updatedUser = user.toObject();
+  delete updatedUser.password;
+  res.status(200).json(updatedUser);
+};
 
 module.exports = {
-    getCurrent: ctrlWrapper(getCurrent),
-    sendSubscriptionEmail: ctrlWrapper(sendSubscriptionEmail),
-    userUpdate: ctrlWrapper(userUpdate),
-}
+  getCurrent: ctrlWrapper(getCurrent),
+  sendSubscriptionEmail: ctrlWrapper(sendSubscriptionEmail),
+  userUpdate: ctrlWrapper(userUpdate),
+};
