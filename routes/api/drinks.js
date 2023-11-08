2562e60344242a5f9c2e20397c901e53a1b895ad
Dmytro Stas <dima7stas777@gmail.com>
2023-11-07 23:06:32 -0500

    authenticate middleware was removed from /own/remove route

--- routes/api/drinks.js
+++ routes/api/drinks.js
@@ -1,25 +1,40 @@
-const express = require('express');
-const { getAllDrinksMainPage, getDrinkById, getFilteredDrinks, getOwnRecipes, removeOwnRecipe,addToFavorite, getFavorite, removeFromFavorite, addOwnDrink, getPopularDrinks  } = require('../../controllers/drinks')
-const { validateBody,authenticate, upload } = require('../../middlewares');
-const {addOwnDrinkJoiSchema} = require("../../models/recipes");
+const express = require("express");
+const {
+  getAllDrinksMainPage,
+  getDrinkById,
+  getFilteredDrinks,
+  getOwnRecipes,
+  removeOwnRecipe,
+  addToFavorite,
+  getFavorite,
+  removeFromFavorite,
+  addOwnDrink,
+  getPopularDrinks,
+} = require("../../controllers/drinks");
+const { validateBody, authenticate, upload } = require("../../middlewares");
+const { addOwnDrinkJoiSchema } = require("../../models/recipes");
 
 const router = express.Router();
 
 router.use(authenticate);
 
-router.get('/mainpage', getAllDrinksMainPage); 
-router.get('/search', getFilteredDrinks); 
-router.get('/popular', getPopularDrinks); 
+router.get("/mainpage", getAllDrinksMainPage);
+router.get("/search", getFilteredDrinks);
+router.get("/popular", getPopularDrinks);
 
+router.get("/own", getOwnRecipes);
+router.post(
+  "/own/add",
+  upload.single("drinkThumb"),
+  validateBody(addOwnDrinkJoiSchema),
+  addOwnDrink
+);
+router.delete("/own/remove", removeOwnRecipe);
 
-router.get('/own', getOwnRecipes); 
-router.post('/own/add', upload.single('drinkThumb'), validateBody(addOwnDrinkJoiSchema), addOwnDrink)  
-router.delete("/own/remove", authenticate, removeOwnRecipe);
-
-router.get("/favorite", getFavorite); 
-router.post("/favorite/add", addToFavorite); 
+router.get("/favorite", getFavorite);
+router.post("/favorite/add", addToFavorite);
 router.delete("/favorite/remove", removeFromFavorite);
 
-router.get('/:id', getDrinkById); 
+router.get("/:id", getDrinkById);
 
 module.exports = router;
